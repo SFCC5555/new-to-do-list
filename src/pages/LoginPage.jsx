e04d1a323af957b00b8f94e95c7bf12de1071d21
sfcc5555 <sfcc.5555@gmail.com>
2024-02-10 02:28:24 -0500

    update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -53,7 +53,7 @@ const LoginPage = () => {
       const login = await postRequest(formData, "login");
       if (login.status) {
         const token = await login.data.user.token;
-        Cookies.set("token", token);
+        Cookies.set("token", token, { domain: "localhost"});
         console.log("TOKEN: ", Cookies.get("token"));
         const profile = await getRequest("profile");
         dispatch(updateProfile(profile));
